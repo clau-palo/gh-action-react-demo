33a5b9b73b94e8c034b022e295b2e9e17e6fdad1
clau-palo <cserrano@palo-it.com>
2025-05-29 17:10:29 -0400

    source control from github.dev

--- src/components/MainContent.jsx
+++ src/components/MainContent.jsx
@@ -8,7 +8,7 @@ function MainContent() {
   function toggleHelp() {
     setHelpVisible((isVisible) => !isVisible);
   }
-
+// un comment
   return (
     <main>
       <button onClick={toggleHelp}>{helpVisible ? 'Hide' : 'Show'} Help</button>
